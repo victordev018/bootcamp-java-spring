a774f222f5ca1daf4d9a493ed174b9bef8b9e996
João VIctor <joawrt8520@gmail.com>
2024-08-29 00:07:12 -0300

    feat: finalizando desafio de controle de fluxo

---
+++ src/sintaxe_basica/DesafioControleFluxo/ParametrosInvalidosException.java
@@ -0,0 +1,11 @@
+package sintaxe_basica.DesafioControleFluxo;
+
+public class ParametrosInvalidosException extends RuntimeException{
+
+    private static final long serialVersionUUID = 1L;
+
+    public ParametrosInvalidosException(String message){
+        super(message);
+    }
+
+}
